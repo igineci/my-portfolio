5d402180a897fc0c628ba635b18c2276b296689d
igineci <andjeladjek@gmail.com>
2025-09-01 19:18:55 +0200

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/routes";
 import { Suspense } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function WrappedApp() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <BrowserRouter>
+        <Analytics />
         <AppRoutes />
       </BrowserRouter>
     </Suspense>
